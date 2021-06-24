59cfb0f34024527e706dab96a73c10056e8a4c43
Chris Woodbury <cwoodbury8997@yahoo.com>
2021-06-24 16:44:16 -0400

    updated

---
+++ main.js
@@ -0,0 +1,15 @@
+let monthOfBirth = prompt(" Enter month of birth. ");
+let yearOfBirth = prompt(" Enter year of birth. ");
+let currentYear =  new Date().getFullYear();
+let currentMonth = new Date().getMonth();
+
+
+
+let secsInMonth = 60 * 60 * 24 * 30;
+let secsInYear = secsInMonth * 12;
+let secsInBirthYear = secsInMonth * (12 - monthOfBirth)
+let secsInCurrentYear = secsInMonth * currentMonth
+let secsBetweenYears = secsInYear * (12 - monthOfBirth)
+let secsSinceBirth =  secsInBirthYear + secsInCurrentYear + secsBetweenYears 
+
+document.write(secsSinceBirth);
